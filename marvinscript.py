67ce4102a9a84d67d90072f72215ea007ced66d4
Dnyaneshwar Birajdar <dnyaneshwarbirajdar89@gmail.com>
2025-04-03 19:53:58 +0530

    marvin scirpt

---
+++ marvinscript.py
@@ -0,0 +1,22 @@
+from prefect import flow, deploy
+from prefect.runner.storage import GitRepository
+
+# Define your flow
+@flow
+def my_flow(name):
+    print(f"Hello, {name}!")
+
+# Load the flow from a public Git repository
+my_flow = flow.from_source(
+    source="https://github.com/dnyaneshwar89/prefect-cloud-test",  # Replace with your repository URL
+    entrypoint="marvinscript.py:my_flow",            # Replace with your file and flow function
+)
+
+# Deploy the flow to Prefect Cloud
+if __name__ == "__main__":
+    deployment = deploy(
+        flow=my_flow,
+        name="example-deployment",
+        work_pool_name="test",  # Replace with your work pool name
+    )
+    print("Deployment created:", deployment)
