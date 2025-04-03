b595c0b1c04150dd78cfdea904236db2548688c1
Dnyaneshwar Birajdar <dnyaneshwarbirajdar89@gmail.com>
2025-04-03 20:18:52 +0530

    trying cli

--- simple.py
+++ simple.py
@@ -1,14 +1,14 @@
-from prefect import flow
+from prefect import flow, task
+
+@task
+def my_task(message):
+    print(f"Task message: {message}")
+    return message
 
 @flow
-def my_flow(name):
-    print(f"hello {name}")
+def my_flow(message="Hello, Prefect Cloud!"):
+    result = my_task(message)
+    print(f"Flow result: {result}")
 
 if __name__ == "__main__":
-    my_flow.deploy(
-        "example-deployment",
-        work_pool_name="test",
-        image="prefecthq/prefect:2-latest",
-        build=True,
-        push=True
-    )
+    my_flow()
